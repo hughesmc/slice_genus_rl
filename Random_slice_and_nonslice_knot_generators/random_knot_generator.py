8107179de05bf776dbd9af582442f1a4300de5d1
Mark Hughes <hughesmc@login04.rc.byu.edu>
2021-07-13 11:41:44 -0600

    Changed upper bound on braid length for random knot generator.

--- Random_slice_and_nonslice_knot_generators/random_knot_generator.py
+++ Random_slice_and_nonslice_knot_generators/random_knot_generator.py
@@ -725,7 +725,7 @@ print("Identifier string = ",identifier)
 
 #non_slice_knots=pd.DataFrame(columns=column_names)
 
-max_braid_length=25
+max_braid_length=35
 number_of_braids=10000
 
 with open("output/BraidsInvariants"+identifier+".csv", "w", newline='') as csv_file:
