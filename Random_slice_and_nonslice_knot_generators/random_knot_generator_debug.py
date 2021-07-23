daaf18216dba9b28f8eb2ffe5b4ec5009524f9a9
Mark Hughes <hughes@Marks-Mac-mini.byu.edu>
2021-07-23 16:08:38 -0600

    Adding statement to change directories before computing Khovanov homology.

--- Random_slice_and_nonslice_knot_generators/random_knot_generator_debug.py
+++ Random_slice_and_nonslice_knot_generators/random_knot_generator_debug.py
@@ -839,13 +839,6 @@ with open("output/BraidsInvariants"+identifier+".csv", "w", newline='') as csv_f
 							log_file.write(log)
 							log_file.close()
 							error_counter=error_counter+1
-							os._exit(0)
-							exit()
-							quit()
-							#os.mkdir("~/compute/Git_Repositories/slice-genus-and-band-rank-rl/Random_slice_and_nonslice_knot_generators/logfiles/"+identifier+"_error"+str(error_counter)) 
-							#os.system("cp ~/compute/Git_Repositories/slice-genus-and-band-rank-rl/Random_slice_and_nonslice_knot_generators/tempfiles/invariants"+identifier+".txt ~/compute/Git_Repositories/slice-genus-and-band-rank-rl/Random_slice_and_nonslice_knot_generators/logfiles/"+identifier+"_error"+str(error_counter)+"/")							
-							#os.system("cp ~/compute/Git_Repositories/slice-genus-and-band-rank-rl/Random_slice_and_nonslice_knot_generators/tempfiles/braidword"+identifier+".brd_s0 ~/compute/Git_Repositories/slice-genus-and-band-rank-rl/Random_slice_and_nonslice_knot_generators/logfiles/"+identifier+"_error"+str(error_counter)+"/")
-							#os.system("cp ~/compute/Git_Repositories/slice-genus-and-band-rank-rl/Random_slice_and_nonslice_knot_generators/tempfiles/braidword"+identifier+".brd ~/compute/Git_Repositories/slice-genus-and-band-rank-rl/Random_slice_and_nonslice_knot_generators/logfiles/"+identifier+"_error"+str(error_counter)+"/")
 						os.remove("tempfiles/invariants"+identifier+".txt")
 						writer.writerow(braid_list)
 						if jjj%10==0:
